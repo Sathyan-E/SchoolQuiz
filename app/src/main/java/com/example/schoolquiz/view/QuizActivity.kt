7174b40876372708d92396dad19aed8840db4125
sathyan <sathyan576@gmail.com>
2020-07-29 14:24:09 +0530

    Quiz function is done

--- app/src/main/java/com/example/schoolquiz/view/QuizActivity.kt
+++ app/src/main/java/com/example/schoolquiz/view/QuizActivity.kt
@@ -3,6 +3,9 @@ package com.example.schoolquiz.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.schoolquiz.R
@@ -15,11 +18,17 @@ class QuizActivity : AppCompatActivity() {
     private var questionNumber=0
     private  var totalQuestions:Int =0
     private var questionList:ArrayList<QuestionDetail> = ArrayList()
+    private var score=0
+    private lateinit var correctAnswer:String
+    private lateinit var studentAnswer:String
+    private var displayRightAnswerMessage="Congratulations!! Perfect Answer"
+    private lateinit var displayWrongAnswerMessage:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
 
         quizviewModel=ViewModelProvider(this).get(QuizActivityViewModel::class.java)
+
         val categoryId=intent.getStringExtra("category_id")
         val amount=intent.getStringExtra("amount")
         val type= intent.getStringExtra("type")
@@ -30,18 +39,37 @@ class QuizActivity : AppCompatActivity() {
         quizviewModel.questionList.observe(this, Observer {
             questionList.clear()
             questionNumber=0
+            score=0
             questionList.addAll(it)
             totalQuestions=questionList.size
             updateUi(questionNumber)
         })
+
+       /**
+        answer_radio_group.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{answer_radio_group,i ->
+            if (i==R.id.option_A){
+                Toast.makeText(this,"Option A",Toast.LENGTH_SHORT).show()
+            }
+            if (i==R.id.option_B){
+                Toast.makeText(this,"Option B",Toast.LENGTH_SHORT).show()
+            }
+            if (i==R.id.option_C){
+                Toast.makeText(this,"Option C",Toast.LENGTH_SHORT).show()
+            }
+            if (i==R.id.option_D){
+                Toast.makeText(this,"Option D",Toast.LENGTH_SHORT).show()
+            }
+        })
+        **/
 
     }
 
     fun updateUi(position:Int){
 
         question_textview.text =questionList.get(position).question
-        var optionList=ArrayList<String>()
+        val optionList=ArrayList<String>()
         optionList.addAll(questionList.get(position).incorrectAnswers!!)
+        correctAnswer=questionList.get(position).correctAnswer!!
         optionList.add(questionList.get(position).correctAnswer!!)
         // optionList.sortBy { it.label.toString() }
         option_A.text=optionList.get(0)
@@ -54,13 +82,40 @@ class QuizActivity : AppCompatActivity() {
     fun submitAnswer(view:View){
         questionNumber++
         next_button.isEnabled=true
+        val id= findViewById<RadioButton>(answer_radio_group.checkedRadioButtonId)
+        studentAnswer=id.text.toString()
+
+        if(studentAnswer==correctAnswer){
+            Toast.makeText(this, "Correct Answer!",Toast.LENGTH_SHORT).show()
+            score++
+            result_textview.text=displayRightAnswerMessage
+            result_textview.setTextColor(resources.getColor(R.color.correctAnswer))
+        }else{
+            Toast.makeText(this, "Wrong Answer!$correctAnswer",Toast.LENGTH_SHORT).show()
+            displayWrongAnswerMessage="Wrong Answer! The correct answer is $correctAnswer"
+            result_textview.text=displayWrongAnswerMessage
+            result_textview.setTextColor(resources.getColor(R.color.wrongAnswer))
+        }
+
 
     }
 
     fun goNextQuestion(view:View){
         if (questionNumber<totalQuestions){
             updateUi(questionNumber)
+            view.isEnabled=false
+            answer_radio_group.clearCheck()
+        }
+        else{
+            Toast.makeText(this,"Quiz Ended.Your  score is $score",Toast.LENGTH_SHORT).show()
+            result_textview.text="Quiz Ended.Your  score is $score"
         }
 
+    }
+
+    fun checkAnswer(view: View){
+        if (view is RadioButton){
+
+        }
     }
 }
